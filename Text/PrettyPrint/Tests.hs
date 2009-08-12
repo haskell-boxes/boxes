f47d75b529840c1bb12c02e2460b7f569ce55ce3
Brent Yorgey <byorgey@gmail.com>
2009-08-13 00:33:57 +0200

    the start of some QuickCheck tests
    Ignore-this: 9272f7ab39b55c48c282fbefda111f64
    
    darcs-hash:20090812223357-bd4d7-30d89e103e2374036901e3952008db0a56aba376.gz

--- Text/PrettyPrint/Tests.hs
+++ Text/PrettyPrint/Tests.hs
@@ -1,13 +1,35 @@
 import Test.QuickCheck
 import Text.PrettyPrint.Boxes
 
--- instance Arbitrary Box where
---  arbitrary = oneOf [ 
---                    ] 
+import Control.Monad
+
+instance Arbitrary Alignment where
+  arbitrary = elements [ AlignFirst
+                       , AlignCenter1
+                       , AlignCenter2
+                       , AlignLast
+                       ]
+
+instance Arbitrary Box where
+  arbitrary = do
+    (NonNegative r) <- arbitrary
+    (NonNegative c) <- arbitrary
+    liftM (Box r c) arbitrary
+
+instance Arbitrary Content where
+  arbitrary = oneof [ return Blank
+                    , liftM Text arbitrary
+                    , liftM Row arbitrary
+                    , liftM Col arbitrary
+                    , liftM3 SubBox arbitrary arbitrary arbitrary
+                    ]
 
 -- extensional equivalence for Boxes
 b1 === b2 = render b1 == render b2
 
 prop_render_text s = render (text s) == (s ++ "\n")
 
-prop_empty_right_id b = b <> emptyBox === b
+prop_empty_right_id b = b <> nilBox === b
+prop_empty_left_id b  = nilBox <> b === b
+prop_empty_top_id b   = nilBox // b === b
+prop_empty_bot_id b   = b // nilBox === b
